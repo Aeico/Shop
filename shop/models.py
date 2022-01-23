71c8b885c0f91c1cca9399a5d675dd78ce9a58ac
Aeico <eojwolf@gmail.com>
2022-01-23 17:27:23 +0100

    Added all models and serializers

--- shop/models.py
+++ shop/models.py
@@ -1,19 +1,34 @@
+from pyexpat import model
 from django.db import models
 
 #User model linked with cart and order history
 class User(models.Model):
     user_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
+    currency = models.IntegerField(default=0)
 
 class Item(models.Model):
     item_id = models.IntegerField(primary_key=True)
-    fk_author = models.ForeignKey(User,null=False,on_delete=models.DO_NOTHING)
+    author_fk = models.ForeignKey(User,null=False,on_delete=models.DO_NOTHING)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
     price = models.IntegerField(null=False)
 
 class Order(models.Model):
     order_id = models.IntegerField(primary_key=True)
-    fk_user_id = models.ForeignKey(User,null=False,on_delete=models.DO_NOTHING)
+    user_fk_id = models.ForeignKey(User,null=False,on_delete=models.DO_NOTHING)
     order_number = models.IntegerField(null=False)
 
+class OrderItem(models.Model):
+    order_fk_id = models.ForeignKey(Order,null=False,on_delete=models.DO_NOTHING)
+    item_fk_id = models.ForeignKey(Item,null=False,on_delete=models.DO_NOTHING)
+    quantity = models.IntegerField(default=1)
+
+class Cart(models.Model):
+    fk_user_id = models.ForeignKey(User,null=False,on_delete=models.DO_NOTHING)
+    cart_id = models.IntegerField(primary_key=True)
+
+class CartItem(models.Model):
+    cart_fk_id = models.ForeignKey(Cart,null=False,on_delete=models.DO_NOTHING)
+    item_fk_id = models.ForeignKey(Item,null=False,on_delete=models.DO_NOTHING)
+    quantity = models.IntegerField(null=False)
--- shop/serializers.py
+++ shop/serializers.py
@@ -1,17 +1,33 @@
 from rest_framework import serializers
-from shop.models import Order, User, Item
+from shop.models import Cart, CartItem, Order, OrderItem, User, Item
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta: 
         model = User
-        fields = ['user_id','name']
+        fields = ['user_id','name','currency']
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ['item_id','fk_author','name','description','price']
+        fields = ['item_id','author_fk','name','description','price']
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ['order_id','order_number','fk_user_id']
+        fields = ['order_id','order_number','user_fk_id']
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['order_fk_id','item_fk_id','quantity']
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ['user_fk_id','cart_id']
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['cart_fk_id','item_fk_id','quantity']
