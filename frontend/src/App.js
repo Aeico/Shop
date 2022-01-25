963832c582b5dec7b205f1c953aa858888c6aa20
Aeico <eojwolf@gmail.com>
2022-01-25 22:09:00 +0100

    Working on design and learning tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,47 @@
 import './App.css';
 import GetUser from './Components/GetUser';
 
-function Nav() {
- 
-
+function NavBar() {
   return (
-    <span>stuff</span>
+    <div className='bg-orange-800 shadow-lg'>
+      <ul className='flex h-16 w-50  justify-center  items-center 
+      shadow-lg'>
+        <div className='ml-3 fixed left-0'>
+          <NavText text="Nothing Shop" />
+        </div>
+        <div className='flex items-center justify-center space-x-6 w-50'>
+          <NavText text="Buy" />
+          <NavText text="Create Item" />
+          <NavText text="Order History" />
+          <NavText text="Your Items" />
+        </div>
+      </ul>
+    </div>
+  )
+}
+
+function NavText({ text }) {
+  return (
+    <li className='nav-text'>{text}</li>
+  )
+}
+
+function Footer() {
+  return (
+    <div className='bg-gray-200 h-10 flex items-center justify-center'>About</div>
   )
 }
 
 function App() {
   return (
-    <div>
-      <nav className="Nav"><Nav/></nav>
-      <header className="App-header">
-      <GetUser/>
+    <div className='font-sans text-black font-bold text-2xl'>
+      <header>
+        <NavBar></NavBar>
       </header>
-      <h1 className="text-blue-500 text-ellipsis text-center">
-      Hello world!
-      </h1>
-      <footer>
-        
-      </footer>
+      <body className='w-screen h-screen'>
+        <GetUser />
+        <Footer />
+      </body>
     </div>
   );
 }
