1815465f2f0eb1649afad65f73b875819263c387
Aeico <eojwolf@gmail.com>
2022-01-29 00:45:36 +0100

    Post works but needs put

--- frontend/src/Components/GetUser.js
+++ frontend/src/Components/GetUser.js
@@ -8,21 +8,15 @@ import Cart from './Cart';
 //http://127.0.0.1:8000/user/1
 //https://joakimjlsv.pythonanywhere.com/user/1/
 
-export function GetUser({ cartClicked, cartToggled }) {
-  const [get, setGet] = React.useState(null);
-  React.useEffect(() => {
-    axios.get('http://127.0.0.1:8000/user/1/')
-      .then((response) => {
-        setGet(response.data)
-      });
-  }, []);
+export function GetUser({ cartClicked, cartToggled, cartInfo, getCur }) {
+  var cartClassName = cartToggled ? 'cart-popup scale-100':'cart-popup scale-0'
 
-  var cart = <div className={cartToggled ? 'cart-popup '+ 'scale-100':'cart-popup '+ 'scale-0' }>It Wont</div>
+  var cart = <Cart cartInfo={cartInfo} cartClassName={cartClassName}></Cart>
 
-  if (!get) return <p>Loading</p>;
+  if (!getCur) return <p>Loading</p>;
   return (
     <div className='h-full w-full p-6 bg-orange-700 items-center text-3xl shadow-lg'>
-      <p>Hello {get.name}!</p>
+      <p>Hello {getCur.name}!</p>
       <div className='pt-5 inline-flex items-center'>Your
         <div className='w-20 h-20 inline-flex pt-1'>
           <Canvas camera={{ position: [0, 0, 2] }}>
@@ -31,7 +25,7 @@ export function GetUser({ cartClicked, cartToggled }) {
             <Currency />
           </Canvas>
         </div>
-        : {get.currency}
+        : {getCur.currency}
       </div>
       <div className='group'>
         <button onClick={cartClicked} key={cartToggled} className='bg-gray-300 rounded-3xl p-2 font-bold'>Your cart</button>
