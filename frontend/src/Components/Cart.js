44b386af0f238edb8e396603129b2aef02356ffa
Aeico <eojwolf@gmail.com>
2022-02-05 18:42:53 +0100

    Cart Item can increase quantity

--- frontend/src/Components/Cart.js
+++ frontend/src/Components/Cart.js
@@ -4,18 +4,30 @@ import React from "react";
 // provides a cart that as of now can have one item and increase and decrease quantity
 
 export function Cart({ cartInfo, setCartInfo }) {
-  const lowerQuantity = () => {
-    setCartInfo({ name: cartInfo.name, quantity: cartInfo.quantity - 1 })
+  const lowerQuantity = (item) => {
+    var newCart = cartInfo
+    newCart.map(mapItem => {
+      if (mapItem.item_id == item.item_id) {
+        mapItem.quantity -= 1;
+      }
+    })
+    setCartInfo(newCart)
   }
 
-  const increaseQuantity = () => {
-    setCartInfo({ name: cartInfo.name, quantity: cartInfo.quantity + 1 })
+  const increaseQuantity = (item) => {
+    var newCart = cartInfo
+    newCart.map(mapItem => {
+      if (mapItem.item_id == item.item_id) {
+        mapItem.quantity += 1;
+      }
+    })
+    setCartInfo(newCart)
   }
 
   var cartItems = cartInfo.map(item =>
     <div key={item.item_id}>
       <h2>{item.name}</h2>
-      <div className='justify-center'> <button onClick={lowerQuantity}>-</button>{item.quantity}<button onClick={increaseQuantity}>+</button></div>
+      <div className='justify-center'> <button onClick={(e) => lowerQuantity(item, e)}>-</button>{item.quantity}<button onClick={(e) => increaseQuantity(item, e)}>+</button></div>
     </div>)
   return (
     cartItems
